a9023153c1dcb4cec68acd8378a8f35e75efa1e7
Reid Belton <belton.reid@gmail.com>
2014-09-20 15:43:25 -0700

    Initial Commit

---
+++ AttrStringBugDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AttrStringBugDemo
+//
+//  Created by reidbelton on 9/20/14.
+//  Copyright (c) 2014 Reid Belton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AttrStringBugDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AttrStringBugDemo
+//
+//  Created by reidbelton on 9/20/14.
+//  Copyright (c) 2014 Reid Belton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
